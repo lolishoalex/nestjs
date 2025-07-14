a212a5deb02a53bac54a89de891b7c97bce0949f
loli <shapilovka@gmail.com>
2025-07-14 18:34:26 +0200

    created movie dto foulder, edited movie entity, movie controller, movie service

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,57 +1,18 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Headers,
-  Param,
-  Post,
-  Query,
-  Req,
-  Res,
-} from '@nestjs/common';
-import type { Request, Response } from 'express';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { MovieService } from './movie.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MovieController {
-  @Get()
-  findAll(@Query() query: any) {
-    return JSON.stringify(query);
-  }
+  constructor(private readonly movieService: MovieService) {}
 
-  @Get(':id')
-  findById(@Param('id') id: string) {
-    return { id };
+  @Get()
+  findAll() {
+    return this.movieService.findAll();
   }
 
   @Post()
-  create(@Body() body: { title: string; genre: string }) {
-    return body;
-  }
-
-  @Get('headers')
-  getHeaders(@Headers() headers: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return headers;
-  }
-
-  @Get('user-agent')
-  getUserAgent(@Headers('user-agent') userAgent: string) {
-    return { userAgent };
-  }
-
-  @Get('request')
-  getRequestDetails(@Req() req: Request) {
-    return {
-      method: req.method,
-      url: req.url,
-      headers: req.headers,
-      query: req.query,
-      params: req.params,
-    };
-  }
-
-  @Get('response')
-  getResponseDetails(@Res() res: Response) {
-    res.status(201).json({ message: 'Hello' });
+  create(@Body() dto: CreateMovieDto) {
+    return this.movieService.create(dto);
   }
 }
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MovieEntity } from './entities/movie.entity';
 import { Repository } from 'typeorm';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Injectable()
 export class MovieService {
@@ -11,6 +12,23 @@ export class MovieService {
   ) {}
 
   async findAll(): Promise<MovieEntity[]> {
-    return await this.movieRepository.find();
+    return await this.movieRepository.find({
+      where: {
+        isPublic: true,
+      },
+      order: {
+        createdAt: 'desc',
+      },
+      select: {
+        id: true,
+        title: true,
+      },
+    });
+  }
+
+  async create(dto: CreateMovieDto): Promise<MovieEntity> {
+    const movie = this.movieRepository.create(dto);
+
+    return await this.movieRepository.save(movie);
   }
 }
