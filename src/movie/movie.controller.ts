ac513fb31e7479b59b86ef343dd51ea930109fc8
loli <shapilovka@gmail.com>
2025-08-05 18:34:22 +0200

    learning decorators fot methods

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  //ApiBody,
+  //ApiHeader,
+  ApiOperation,
+  //ApiParam,
+  //ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('Movie')
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
+  @ApiOperation({
+    summary: 'Get films list',
+    description: 'Calling back all films list',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Found films',
+  })
   @Get()
   findAll() {
     return [
@@ -20,4 +44,52 @@ export class MovieController {
       },
     ];
   }
+
+  @ApiOperation({
+    summary: 'Get film by ID',
+    description: 'Calling back film info',
+  })
+  // @ApiParam({
+  //   name: 'id',
+  //   type: 'string',
+  //   description: 'Film ID',
+  // })
+  // @ApiQuery({
+  //   name: 'year',
+  //   type: 'number',
+  //   description: 'Filter by year',
+  // })
+  // @ApiHeader({
+  //   name: 'X-Auth-Token',
+  //   description: 'autorisation token',
+  // })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Found film',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not Found film',
+  })
+  @Get(':id')
+  findById(@Param('id') id: string, @Query('year') year: number) {
+    return {
+      id: 1,
+      title: 'Fight Club',
+    };
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Create film',
+  })
+  // @ApiBody({
+  //   schema: {
+  //     type: 'object',
+  //     properties: {
+  //       title: { type: 'string', example: 'Fight Club' },
+  //     },
+  //   },
+  // })
+  create() {}
 }
