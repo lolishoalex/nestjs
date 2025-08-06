3559c6303ceb5388705a83fd8bb7a73aaf5ba18d
loli <shapilovka@gmail.com>
2025-08-06 16:24:15 +0200

    practical project: JWT auth in nest js

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { MovieModule } from './movie/movie.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,7 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
-    MovieModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { logger } from './common/middlewares/logger.middleware';
-import { ResponseInteceptor } from './common/interceptors/response.interceptor';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { MovieModule } from './movie/movie.module';
-import { MovieResponse } from './movie/dto/movie.dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.useGlobalInterceptors(new ResponseInteceptor());
-
-  app.useGlobalFilters(new AllExceptionsFilter());
-
   const config = new DocumentBuilder()
     .setTitle('Nest course API')
     .setDescription('API documentation for Nest course')
     .setVersion('1.0.0')
     .setContact('Coder Team', 'https://coderteam.com', 'support@coderteam.com')
-    //.addBearerAuth()
     .setLicense('MIT', 'https://github.com')
-    //.setTermsOfService('')
+
     .build();
 
   const document = SwaggerModule.createDocument(app, config, {
-    include: [MovieModule],
-    //deepScanRoutes: true,
-    extraModels: [MovieResponse],
     operationIdFactory: (controllerKey, methodKey) =>
       `${controllerKey}-${methodKey}`,
   });
@@ -40,8 +27,6 @@ async function bootstrap() {
     yamlDocumentUrl: '/swagger.yaml',
     customSiteTitle: 'Nest JS API docs',
   });
-
-  app.use(logger);
 
   await app.listen(3000);
 }
