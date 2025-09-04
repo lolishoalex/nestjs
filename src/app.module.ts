5545acec8bd7f0f4c21c62ce87e258325640f699
loli <shapilovka@gmail.com>
2025-09-04 13:59:25 +0200

    Testing jest: E2E, testing passed successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArtistModule } from './artist/artist.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ArtistController } from './artist/artist.controller';
+import { ArtistService } from './artist/artist.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
@@ -11,5 +16,7 @@ import { ArtistModule } from './artist/artist.module';
     PrismaModule,
     ArtistModule,
   ],
+  controllers: [AppController, ArtistController],
+  providers: [AppService, ArtistService, PrismaService],
 })
 export class AppModule {}
