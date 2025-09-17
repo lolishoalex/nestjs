b7bd246085758fec971830be7c9da564a49173f3
loli <shapilovka@gmail.com>
2025-09-17 13:57:42 +0200

    work with versioning

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('artists/:id')
-  /*   findAll() {
+  @Get()
+  getHello(): string {
     return this.appService.hello();
-  } */
+  }
+
+  @Version('2')
+  @Get('artists/:id')
   async getArtist(@Param('id') id: string) {
     return this.appService.getArtist(id);
   }
+
   @Get('albums/:id')
   async getAlbum(@Param('id') id: string) {
     return this.appService.getAlbum(id);
